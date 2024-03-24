1834e251afd39755c85e4617832b105f37598524
EC2 Default User <ec2-user@ip-172-31-4-248.ap-northeast-1.compute.internal>
2024-03-24 12:51:21 +0000

    boostrap new

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  devise_for :users
   get "about" => "homes#about"
+  devise_for :users
   resources :comments, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :favorites, only: [:index], param: :comment_id
     resources :responses, only: [:create, :destroy]
   end
+
   resources :users, only: [:show, :edit, :update]
   devise_scope :user do
     post 'users/guest_sign_in', to:'users/sessions#guest_sign_in', as: :guest_sign_in
